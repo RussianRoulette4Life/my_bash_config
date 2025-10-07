229b2bbd1c240acc581be3019ff98d72bc5d465f
Руслан <petrushin-ruslanrus@yandex.ru>
2025-10-07 20:00:35 +0300

    ща можно shift-tab

--- scripts/init_set_style.sh
+++ scripts/init_set_style.sh
@@ -2,6 +2,7 @@
 export PROMPT_COMMAND='PS1="$(bash -c $RESPIO_CONFIG_PATH/themes/$RESPIO_THEME)"'
 bind 'set show-all-if-ambiguous on'
 bind 'TAB:menu-complete'
+bind '"\e[Z": menu-complete-backward'
 alias ls="ls --color"
 alias l="ls"
 alias ll="ls -la"
