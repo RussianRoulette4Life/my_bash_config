371459f809e1d53a0007865287a3dfbd31b554c3
Ruslan <petrushin-ruslanrus@yandex.ru>
2025-10-11 10:23:17 +0300

    добавил просморт заряда батареи

--- default/settings.sh
+++ default/settings.sh
@@ -5,8 +5,13 @@
 # который экспортирует TOP_PROMPT и MID_PROMPT в RESPIO_CONFIG_PATH/themes/
 export RESPIO_THEME=default
 
-# работает для тем с длинными стрелочками (w - wide, c - compact, u - ultracompact)
+
+# СЕКЦИЯ ДЛЯ СТАНДАРТНОЙ ТЕМЫ!
+## работает для тем с длинными стрелочками (w - wide, c - compact, u - ultracompact)
 export RESPIO_COMPACT=u
 
+
+# СЕКЦИЯ ДЛЯ ОБОИХ ТЕМ!
+export SHOW_BAT_CHARGE=yes
 
 
